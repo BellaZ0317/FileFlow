2da95ffe2cd44dddb430b2d1d435a77eb0c8d139
Bella Zhong <122234271+BellaZ0317@users.noreply.github.com>
2023-09-08 15:58:05 +0800

    celery tasks and react models

--- gulpfile.js
+++ gulpfile.js
@@ -13,16 +13,18 @@ var gulp = require('gulp'),
 gulp.task('transform', function () {
   return gulp.src('./fileflow/static/jsx/app.js')
     .pipe(browserify({transform: ['reactify']}))
-    .pipe(gulp.dest('./project/static/scripts/js'))
+    .pipe(gulp.dest('./fileflow/static/js'))
     .pipe(size());
 });
 
+/*
 gulp.task('clean', function () {
   return gulp.src(['./fileflow/static/scripts/js'], {read: false})
     .pipe(clean());
 });
+*/
 
-gulp.task('default', ['clean'], function () {
+gulp.task('default', /*['clean'],*/ function () {
   gulp.start('transform');
   gulp.watch('./fileflow/static/jsx/app.js', ['transform']);
 });
